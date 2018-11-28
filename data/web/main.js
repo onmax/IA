4c68c85c5dada25349979d281206abb15d78876e
max <maximo.garcia.martinez@alumnos.upm.es>
2018-11-28 23:07:03 +0100

    Updated data of stations

--- data/web/main.js
+++ data/web/main.js
@@ -1,18 +1,25 @@
-const stations = ["Beruni","Tinchlik", "Chorsu","Gafur Golum", "Pakhtakor", "Uzbekistan","Kosmonavtlar", "Oybek", "Toskent", "Mashinasozlar", "Dostlik", "Olmazor", "Chilonzor", "Mirzo Ulugbek", " Novza", "MilliyBog", "Bunyodkor", "Mustakillik Maydoni", "Amir Temur Hiyoboni", "Khamid Alimjan", "Pushkin", "Buyuj Ipak Yuli", "Shakhirston", "Bodomzor", "Minor", "Abdulla Kodiriy", "Alisher Navoi", "Ming Urik", "Yunus Rajably"]
+const stations = ["Beruni", "Tinchlik", "Chorsu", "Gafur Golum", "Pakhtakor", "Uzbekistan", "Kosmonavtlar", "Oybek", "Toskent", "Mashinasozlar", "Dostlik", "Olmazor", "Chilonzor", "Mirzo Ulugbek", " Novza", "MilliyBog", "Bunyodkor", "Mustakillik Maydoni", "Amir Temur Hiyoboni", "Khamid Alimjan", "Pushkin", "Buyuj Ipak Yuli", "Shakhirston", "Bodomzor", "Minor", "Abdulla Kodiriy", "Alisher Navoi", "Ming Urik", "Yunus Rajably"]
 console.table(stations)
 const img = document.querySelector('img')
 
 
 res = []
 img.addEventListener('click', (e) => {
-    x = e.clientX;
-    y = e.clientY;
+
+    x = e.offsetX;
+    y = img.offsetHeight - e.offsetY;
+
     const index = prompt("Indice de la estacion?");
-    if(index >= 0 && index < stations.length){
+    if (index >= 0 && index < stations.length) {
         station = stations[index];
-        res.push({x,y,station});
+        res.push({
+            x,
+            y,
+            station
+        });
         console.log(res);
-    }else{
+        document.getElementById('data').innerHTML = JSON.stringify(res);
+    } else {
         console.error("NO existe ese indice, vuelve a intentarlo.")
     }
 
